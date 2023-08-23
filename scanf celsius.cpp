07cf093d17626999fc6c9214493d3f21f74547a4
Suhani8650 <142657135+Suhani8650@users.noreply.github.com>
2023-08-23 11:53:34 +0530

    Add files via upload
    kindly rectify my mistake in this code.

---
+++ scanf celsius.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	float centigrade;
+	float farenheit;
+	
+	scanf("%d", &centigrade);
+	farenheit== ((9*centigrade)/5)+32;
+	
+	printf("the value of %d centigrade ", centigrade);
+	printf("is equals to %d farenheit",farenheit);
+}
